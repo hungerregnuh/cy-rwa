63b13b15e24a7ca5421ef9ea85f2404a2dcccfc3
Hank P <simplexity.solutions@gmail.com>
2026-02-24 15:09:09 +0000

    Update TransactionCreateStepTwo.tsx

--- src/components/TransactionCreateStepTwo.tsx
+++ src/components/TransactionCreateStepTwo.tsx
@@ -136,10 +136,10 @@ const TransactionCreateStepTwo: React.FC<TransactionCreateStepTwoProps> = ({
                     fullWidth
                     required
                     autoFocus
-                    id={"transaction-create-amount-inputt"}
+                    id={"transaction-create-amount-inputtt"}
                     type="text"
                     placeholder="Amount"
-                    data-test={"transaction-create-amount-inputt"}
+                    data-test={"transaction-create-amount-inputtt"}
                     error={(touched || value !== initialValue) && Boolean(error)}
                     helperText={touched || value !== initialValue ? error : ""}
                     InputProps={{
